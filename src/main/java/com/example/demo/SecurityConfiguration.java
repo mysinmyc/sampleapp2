209e58a7354cdfb7298101d250a30c0c051efc3d
Alessandro Ventura <mysinmyc@users.noreply.github.com>
2021-03-06 22:36:45 +0100

    wip

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -1,13 +1,41 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication
-public class DemoApplication {
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+	@Value("${appUser:admin}")
+	String appUser;
+
+	@Value("${appUserPassword:passw0rd}")
+	String appUserPassword;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().formLogin()
+				.loginPage("/public/login.html").loginProcessingUrl("/public/login")
+				.defaultSuccessUrl("/index.html", true).permitAll();
+	}
+
+	@Bean
+	@Override
+	protected UserDetailsService userDetailsService() {
+		UserDetails user = User.withDefaultPasswordEncoder().username(appUser).password(appUserPassword).roles("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
 	}
 
 }
